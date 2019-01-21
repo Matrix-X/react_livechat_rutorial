76d1a54b8a8c8d7d69b347e803cc377e1626894c
hbj418 <hbj418@gmail.com>
2019-01-21 18:21:23 +0800

    init a project and config push configure

---
+++ src/config.js
@@ -0,0 +1,7 @@
+
+// console.log(process.env);
+const tokenUrl = process.env.REACT_APP_PUSHER_APP_URL;
+const instanceLocator = process.env.REACT_APP_PUSHER_APP_KEY;
+// console.log(tokenUrl, instanceLocator);
+
+export { tokenUrl, instanceLocator }
